dbe1d3cc9a650b90785c7469b548af9cee9f8477
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-13 10:27:35 +0530

    [Jyoti Rao] Perform Addition functionality

---
+++ BasicC-SharpPrograms248/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicC_SharpPrograms248
+{
+    /// <summary>
+    /// Calculator class created to perform arithmetic operations
+    /// </summary>
+    public class Calculator
+    {
+        // instance variables
+        public int first;
+        public int second;
+        public double result;
+
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        public Calculator(int firstNumber, int secondNumber)
+        {
+            first = firstNumber;
+            second = secondNumber;
+        }
+        /// <summary>
+        /// Addition of two numbers to get sum
+        /// </summary>
+        public void Addition()
+        {
+            result = first + second;
+            Console.WriteLine("Sum : " + result);
+        }
+    }
+}
---
+++ BasicC-SharpPrograms248/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicC_SharpPrograms248
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the basics c-sharp programs");
+            Console.WriteLine("Enter first number: ");
+            int first = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int second = Convert.ToInt16(Console.ReadLine());
+            Calculator calculator1 = new Calculator(first, second);
+            calculator1.Addition();
+        }
+    }
+}
