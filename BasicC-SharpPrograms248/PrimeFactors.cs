c98d0174e4ae3fb5da3f09d94dc84770e17a580d
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-18 13:11:40 +0530

    [Jyoti Rao] Print the prime factors of number N

---
+++ BasicC-SharpPrograms248/PrimeFactors.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicC_SharpPrograms248
+{
+    /// <summary>
+    /// PrimeFactors class created to Print the prime factors of number N.
+    /// </summary>
+    public class PrimeFactors
+    {
+        //static variable
+        public static int number;
+
+        /// <summary>
+        /// Use static method to Print the prime factors of number N.
+        /// </summary>
+        public static void PrintPrimeFactors()
+        {
+            Console.Write("Enter number which you want to find prime factor: ");
+            number=Convert.ToInt32(Console.ReadLine());
+            for(int i=2; i<=number; i++)
+            {
+                while(number%i==0)
+                {
+                    number = number / i;
+                    Console.WriteLine(i);
+                }
+            }
+            if(number>2)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
--- BasicC-SharpPrograms248/Program.cs
+++ BasicC-SharpPrograms248/Program.cs
@@ -6,69 +6,86 @@ namespace BasicC_SharpPrograms248
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the basics c-sharp programs");
-            Console.WriteLine("Select given options");
-            Console.WriteLine("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.WeekDays\n6.Find leap year\n7.FindPowerOfTwo\n8.SwapTwoNumbers\n9.CheckAlphabetIsVowelOrConsonant\n10.Exit\n");
-            int option = Convert.ToInt16(Console.ReadLine());
-            switch (option)
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("Enter first number: ");
-                    int first = Convert.ToInt16(Console.ReadLine());
-                    Console.WriteLine("Enter second number: ");
-                    int second = Convert.ToInt16(Console.ReadLine());
-                    Calculator calculator1 = new Calculator(first, second);
-                    calculator1.Addition();
-                    break;
-                case 2:
-                    Console.WriteLine("Enter first number: ");
-                    int third = Convert.ToInt16(Console.ReadLine());
-                    Console.WriteLine("Enter second number: ");
-                    int fourth = Convert.ToInt16(Console.ReadLine());
-                    Calculator calculator2 = new Calculator(third, fourth);
-                    calculator2.Substraction();
-                    break;
-                case 3:
-                    Console.WriteLine("Enter first number: ");
-                    int fithnum = Convert.ToInt16(Console.ReadLine());
-                    Console.WriteLine("Enter second number: ");
-                    int sixthnum = Convert.ToInt16(Console.ReadLine());
-                    Calculator calculator3 = new Calculator(fithnum, sixthnum);
-                    calculator3.Multiplication();
-                    break;
-                case 4:
-                    Console.WriteLine("Enter first number: ");
-                    int number1 = Convert.ToInt16(Console.ReadLine());
-                    Console.WriteLine("Enter second number: ");
-                    int number2 = Convert.ToInt16(Console.ReadLine());
-                    Calculator calculator4 = new Calculator(number1, number2);
-                    calculator4.Division();
-                    break;
-                case 5:
-                    Week.WeekDays();
-                    break;
-                case 6:
-                    LeapYear.FindLeapYear();
-                    break;
-                case 7:
-                    Powerof2.FindPowerOfTwo();
-                    break;
-                case 8:
-                    SwapNumbers.SwapTwoNumbers();
-                    break;
-                case 9:
-                    Console.WriteLine("Enter character for checking Vowel or Consonant");
-                    char vowel = Convert.ToChar(Console.ReadLine());
-                    Alphabet alphabet = new Alphabet(vowel);
-                    alphabet.CheckAlphabetIsVowelOrConsonant();
-                    break;
-                case 10:
-                    break;
-                default:
-                    Console.WriteLine("Select valid options only ");
-                    break;
+                Console.WriteLine("Welcome to the basics c-sharp programs");
+                Console.WriteLine("Select given options");
+                Console.WriteLine("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.WeekDays\n6.Find leap year\n7.FindPowerOfTwo\n8.SwapTwoNumbers\n9.CheckAlphabetIsVowelOrConsonant\n10.FindPrimeFactors\n11.Exit\n");
+                int option = Convert.ToInt16(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Enter first number: ");
+                        int first = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Enter second number: ");
+                        int second = Convert.ToInt16(Console.ReadLine());
+                        Calculator calculator1 = new Calculator(first, second);
+                        calculator1.Addition();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Enter first number: ");
+                        int third = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Enter second number: ");
+                        int fourth = Convert.ToInt16(Console.ReadLine());
+                        Calculator calculator2 = new Calculator(third, fourth);
+                        calculator2.Substraction();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Enter first number: ");
+                        int fithnum = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Enter second number: ");
+                        int sixthnum = Convert.ToInt16(Console.ReadLine());
+                        Calculator calculator3 = new Calculator(fithnum, sixthnum);
+                        calculator3.Multiplication();
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter first number: ");
+                        int number1 = Convert.ToInt16(Console.ReadLine());
+                        Console.WriteLine("Enter second number: ");
+                        int number2 = Convert.ToInt16(Console.ReadLine());
+                        Calculator calculator4 = new Calculator(number1, number2);
+                        calculator4.Division();
+                        break;
+                    case 5:
+                        Console.Clear();
+                        Week.WeekDays();
+                        break;
+                    case 6:
+                        Console.Clear();
+                        LeapYear.FindLeapYear();
+                        break;
+                    case 7:
+                        Console.Clear();
+                        Powerof2.FindPowerOfTwo();
+                        break;
+                    case 8:
+                        Console.Clear();
+                        SwapNumbers.SwapTwoNumbers();
+                        break;
+                    case 9:
+                        Console.Clear();
+                        Console.WriteLine("Enter character for checking Vowel or Consonant");
+                        char vowel = Convert.ToChar(Console.ReadLine());
+                        Alphabet alphabet = new Alphabet(vowel);
+                        alphabet.CheckAlphabetIsVowelOrConsonant();
+                        break;
+                    case 10:
+                        Console.Clear();
+                        PrimeFactors.PrintPrimeFactors();
+                        break;
+                    case 11:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Select valid options only ");
+                        break;
+                }
+                Console.ReadLine();
             }
-            Console.ReadLine();
         }
     }
 }
