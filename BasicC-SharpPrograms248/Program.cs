9569d1f7a1da51ebabf76c1b382a6a2ef2a38399
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-01-18 13:39:21 +0530

    [Jyoti Rao] Constructor: Checking Age for Voting

--- BasicC-SharpPrograms248/Program.cs
+++ BasicC-SharpPrograms248/Program.cs
@@ -10,7 +10,7 @@ namespace BasicC_SharpPrograms248
             {
                 Console.WriteLine("Welcome to the basics c-sharp programs");
                 Console.WriteLine("Select given options");
-                Console.WriteLine("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.WeekDays\n6.Find leap year\n7.FindPowerOfTwo\n8.SwapTwoNumbers\n9.CheckAlphabetIsVowelOrConsonant\n10.FindPrimeFactors\n11.Exit\n");
+                Console.WriteLine("1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.WeekDays\n6.Find leap year\n7.FindPowerOfTwo\n8.SwapTwoNumbers\n9.CheckAlphabetIsVowelOrConsonant\n10.FindPrimeFactors\n11.CheckVotingAge\n12.Exit\n");
                 int option = Convert.ToInt16(Console.ReadLine());
                 switch (option)
                 {
@@ -77,6 +77,11 @@ namespace BasicC_SharpPrograms248
                         PrimeFactors.PrintPrimeFactors();
                         break;
                     case 11:
+                        Console.Clear();
+                        VotingAge votingAge=new VotingAge();
+                        votingAge.CheckVotingAge();
+                        break;
+                    case 12:
                         Console.Clear();
                         break;
                     default:
---
+++ BasicC-SharpPrograms248/VotingAge.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicC_SharpPrograms248
+{
+    /// <summary>
+    /// VotingAge class created to check leap year
+    /// </summary>
+    public class VotingAge
+    {
+        // instance variables
+        public int requiredAge;
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="eligibleAge"></param>
+        public VotingAge(int eligibleAge)
+        {
+            requiredAge = eligibleAge;
+        }
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public VotingAge()
+        {
+            Console.WriteLine("Welcome to the voting age process");
+        }
+        /// <summary>
+        /// Checking Age for Voting
+        /// </summary>
+        public void CheckVotingAge()
+        {
+            Console.Write("Please enter your age: ");
+            requiredAge = Convert.ToInt32(Console.ReadLine());
+            if(requiredAge>18)
+            {
+                Console.WriteLine("Your age are eligible for give vote");
+            }
+            else
+            {
+                Console.WriteLine("Your age are not eligible for give vote because your age is less the 18");
+            }
+        }
+    }
+}
